934bf3dfb73f5280d06bbcf708d27f6d1be1f973
Will Madden <will.madden@playup.com>
2011-07-10 13:39:14 +1000

    Implemented basic REST methods on MenusController.

--- src/rails/app/controllers/menus_controller.rb
+++ src/rails/app/controllers/menus_controller.rb
@@ -7,4 +7,18 @@ class MenusController < ApplicationController
     @menu = Menu.new
   end
   
+  def create
+    @menu = Menu.new( params['menu'] )
+    if @menu.valid?
+      @menu.save!
+      redirect_to menu_path(@menu)
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @menu = Menu.find( params['id'] )
+  end
+  
 end
--- src/rails/spec/controllers/menus_controller_spec.rb
+++ src/rails/spec/controllers/menus_controller_spec.rb
@@ -11,4 +11,68 @@ describe MenusController do
     
   end
   
+  describe '#create' do
+    
+    let(:menu_valid) { false }
+    let(:menu) { mock(:menu, :valid? => menu_valid ) }
+    
+    before :each do
+      Menu.stub(:new).and_return(menu)
+    end
+    
+    it "should call Menu.new with the params from the request" do
+      Menu.should_receive(:new).with({ 'name' => 'Some Menu' }).and_return(menu)
+      post 'create', { 'menu' => { 'name' => 'Some Menu' } }
+    end
+    
+    it "should check that the menu is valid" do
+      Menu.stub(:new).and_return(menu)
+      menu.should_receive(:valid?)
+      post 'create'
+    end
+    
+    describe 'when the Menu is invalid' do
+      let(:menu_valid) { false }
+      
+      it 'it should render #new' do
+        post 'create'
+        response.should render_template('new')
+      end
+    end
+    
+    describe 'when the Menu is valid' do
+      let(:menu_valid) { true }
+      
+      it 'should create the Menu' do
+        menu.should_receive 'save!'
+        post 'create'
+      end
+      
+      it 'should redirect to #show' do
+        menu.stub(:save!)
+        post 'create'
+        response.should redirect_to( menu_path(menu) )
+      end
+    end
+    
+  end
+  
+  describe '#show' do
+    
+    it 'should find the Menu' do
+      Menu.should_receive(:find).with('menu-ID')
+      get 'show', :id => 'menu-ID'
+    end
+    
+    it 'should assign the Menu to @menu' do
+      menu = mock(:menu)
+      Menu.stub(:find).and_return( menu )
+      
+      get 'show', :id => 'menu-ID'
+      
+      assigns(:menu).should == menu
+    end
+    
+  end
+  
 end
