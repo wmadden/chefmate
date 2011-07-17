3a97057f1682d68386dabca5c90c89489195c2e3
Will Madden <will.madden@playup.com>
2011-07-17 13:15:30 +1000

    Can't get Compass to fucking behave.

--- src/sass/config.rb
+++ src/sass/config.rb
@@ -1,12 +1,22 @@
 # Require any additional compass plugins here.
+project_type = :stand_alone
 
 # Set this to the root of your project when deployed:
 http_path = "/"
 
-css_path = "../rails/public/stylesheets"
 sass_path = "."
-images_dir = "../rails/public/images"
-javascripts_dir = "../rails/public/javascripts"
+
+#css_dir = "stylesheets"
+css_path = "../rails/public/stylesheets"
+http_stylesheets_path = "/stylesheets"
+
+#images_dir = "images"
+images_path = "../rails/public/images"
+http_images_path = "/images"
+
+#javascripts_dir = "javascripts"
+javascripts_path = "../rails/public/javascripts"
+http_javascripts_path = "/javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
