9b2dc301c1fd60d70b5628aee05bf79316d6338b
Will Madden <will.madden@playup.com>
2011-08-07 21:17:48 +1000

    Fixed issues in Application and implemented saving to Mongo.

--- src/rover/lib/rover/application.rb
+++ src/rover/lib/rover/application.rb
@@ -13,16 +13,26 @@ module Rover
     def run( site )
       case site
         when "recipes.com.au"
-          scraper = Scrapers::RecipesComAu.new
+          scraper = RecipesComAu::Scraper.new
         else
           puts "Don't know how to scrape #{site}" unless site.nil?
           puts "Usage: wander [site]"
           puts "Known sites:"
           puts "  recipes.com.au"
       end
-
-      scraper.scrape
+      
+      recipes = scraper.scrape
+      recipes.each do |recipe|
+        puts "Fetching recipe '#{recipe.url}'..."
+        recipe.fetch
+        save_recipe( recipe )
+      end
     end
-
+    
+    def save_recipe( recipe )
+      @db ||= Mongo::Connection.new.db("chefmate")
+      @collection ||= @db.collection("recipes")
+      @collection.insert( recipe.data )
+    end
   end
 end
